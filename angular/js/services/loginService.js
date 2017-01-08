e5ea6e524aeab59613dac64d846e0e0d87bfb83d
johnfrades <johnfrades@gmail.com>
2017-01-09 07:33:05 +1300

    c

--- angular/js/services/loginService.js
+++ angular/js/services/loginService.js
@@ -46,37 +46,7 @@ angular.module("main").service("loginService", function($http, $localStorage){
     
  
 //MY PROFILE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    this.profileUpdate = function(address, email, phone, linkedin, website){
-        var updatedProfile = {
-            method: 'POST',
-            url: 'http://localhost:3000/updatemyprofile',
-            dataType: 'json',
-            contentType: 'application/json',
-            data: {
-                id: $localStorage.studentID,
-                address: address,
-                email: email,
-                phone: phone,
-                linkedin: linkedin,
-                website: website
-            }
-        }
-        
-        return $http(updatedProfile)
-            .then(function(response){
-            console.log(response.data);
-            $localStorage.studentAddress = response.data.info.address;
-            $localStorage.studentEmail = response.data.info.email;
-            $localStorage.studentPhone = response.data.info.phone;
-            $localStorage.studentLinkedin = response.data.info.linkedin;
-            $localStorage.studentWebsite = response.data.info.website;
-            return response.data.success;
-        }, function(error){
-            return response.data.success;
-            console.log('ERROR on Update Profile');
-        });
-        
-    }
+
 
     
     
